b57bc430dd77e6327ae1f62eeeaa1596012a08eb
radionomerecords <79709676+radionomerecords@users.noreply.github.com>
2021-02-26 19:24:51 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+// insta
+function insta()
+{
+  window.location.href="https://www.instagram.com/radionomerecords/"
+  
+}
+// twitter
+function twitter()
+{
+  window.location.href="https://twitter.com/RadionomeR"
+  
+}
+//youtube
+function youtube()
+{
+  window.location.href="https://www.youtube.com/channel/UCd2R6tQ-eY1cmMZ02tVqeXQ"
+  
+}
+//facebook
+function facebook()
+{
+  window.location.href="https://www.facebook.com/Radionome-Records-102633688515088/"
+  
+}
+function acilirp()
+{
+  window.location.href=("demo-yolla.html")
+}
